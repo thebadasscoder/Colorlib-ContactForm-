4335463070147a71fdd94503a56ed3a2de6c8531
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2017-09-20 20:16:06 -0400

    add javascrip

---
+++ js/form.js
@@ -0,0 +1,11 @@
+// This is where the input values are being passed and console logged inside your terminal.
+
+function getVal(val){
+	console.log(val);	
+}
+
+//This is where the message the will pop up after submitting the information inside of the contact form. 
+function submitVal(){
+	alert('Thank you for contacting us! We will get back to you within the next 24-48hours. We thank you for your time and patience.');
+}
+
